7254ba8afeccb34cec1cc00c11a7adaad22da584
liuwenyao <Liuwenyao2017@126.com>
2020-11-28 18:12:27 +0800

    登录

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
 import React from 'react';
+import { Form, Input, Button, Checkbox } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 8 },
+  };
+  const tailLayout = {
+    wrapperCol: { offset: 4, span: 16 },
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Form
+          {...layout}
+          name="register"
+          initialValues={{ remember: true }}
+          className="login-form"
         >
-          Learn React
-        </a>
-      </header>
+          <Form.Item
+            label="手机号"
+            name="username"
+            rules={[{ required: true, message: '请输入用户名！' }]}
+          >
+            <Input placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item
+            label="密码"
+            name="password"
+            rules={[{ required: true, message: '请输入密码！' }]}
+          >
+            <Input
+              type="password"
+              placeholder="请输入密码"
+            />
+          </Form.Item>
+          <Form.Item {...tailLayout}>
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox>记住密码</Checkbox>
+            </Form.Item>
+
+            <a className="login-form-forgot" href="">
+              忘记密码
+            </a>
+          </Form.Item>
+
+          <Form.Item {...tailLayout}>
+            <Button type="primary" htmlType="submit">
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
     </div>
   );
 }
